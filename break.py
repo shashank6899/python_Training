f15e7ca6a79cfc2e676dd44fbb249a76ec88c9d7
shashank6899 <70883540+shashank6899@users.noreply.github.com>
2021-09-28 23:04:45 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,8 @@
+for k in range(10):
+    i=10
+    j=20
+    sum=i+j
+    print(k)
+    break
+    print(sum)
+print("hello")
---
+++ range.py
@@ -0,0 +1,6 @@
+#for v in range(10,101,10):
+    #print(v)\
+
+words = ['cat','fish','tiger']
+for w in words:
+    print(w,len(w))
